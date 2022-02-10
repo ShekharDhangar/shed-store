64d185607613b31738696a9f21cca686970faf58
dhangarshekhar <shekhardhangar@yahoo.com>
2022-02-10 11:56:23 +0530

    Refactored: Changed  code to ES6 Syntax
    Refactored: changed code to ES6 syntax

--- authentication/login.js
+++ authentication/login.js
@@ -3,35 +3,17 @@ const signUpForm = document.getElementById('sign-up'),
 trylogIn = document.getElementById('try-login'),
 trySignUp = document.getElementById('try-signup'),
 logInForm = document.getElementById('login');
+const passwordEye = document.querySelector('.password-control');
+const showPasswordInp = document.querySelector('.see-password-input');    
 
 trySignUp.addEventListener('click', changeForm)
 trylogIn.addEventListener('click', changeForm)
- 
+passwordEye.addEventListener('click',show_hide_password)
+
 function changeForm(){
     signUpForm.classList.toggle('none')
     logInForm.classList.toggle('none');
 }
 // see password value 
-let passwordEye = document.querySelector('.password-control');
+const show_hide_password = event => event.target.classList.toggle('view'); (showPasswordInp.getAttribute('type') == 'password')? showPasswordInp.setAttribute('type', 'text') : showPasswordInp.setAttribute('type', 'password');
 
-function show_hide_password(event){
-    let showPasswordInp = document.querySelector('.see-password-input');    
-	if (showPasswordInp.getAttribute('type') == 'password') {
-		event.target.classList.add('view');
-		showPasswordInp.setAttribute('type', 'text');
-	} else {
-		event.target.classList.remove('view');
-		showPasswordInp.setAttribute('type', 'password');
-	}
-}
-
-        
-passwordEye.addEventListener('click',show_hide_password)
-// 
-
-
-
-if (window.matchMedia("(hover: none)")){
-	console.log('mobile')
-}
-window.matchMedia('(hover:none)')?console.log('mobile'):console.log("laptop");
